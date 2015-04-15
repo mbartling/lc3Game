789823ca807ea4eb8f03acd1968554d2d5e547c0
Rigl <grimfate126@gmail.com>
2015-04-15 15:58:48 -0500

    [map] update written. definitely wont work.

--- map.asm
+++ map.asm
@@ -20,9 +20,47 @@ DRAW
 
 
 UpdateElement
-  ;STR R6, R6, #0  ;Push Stack Pointer
-  STR R7, R6, #0   ;Push Return Address
 
+  ; Save registers  
+  STR R0, R6, #0  ; Save R0
+  STR R1, R6, #-1  ; Save R1
+  STR R2, R6, #-2  ; Save R2
+  STR R3, R6, #-3  ; Save R3
+  LDR R0, R6, #1   ; Get y coordinate
+  LDR R1, R6, #2  ; Get x coordinate
+  
+  ; Compute final store address as: y * 9 + x
+  AND R2, R2, #0
+  ADD R2, R2, #9
+
+  ; Multiply y (R0) by 9 and store in R3
+  AND R3, R3, #0
+
+MULT	ADD R3, R3, R0
+	ADD R2, R2, #-1
+	BRp MULT	
+
+  ; Add offset (x)
+  ADD R3, R3, R1
+
+  ; Add MAP location offset
+  LEA R0, MAP
+  ADD R3, R3, R0
+
+  ; Restore R0
+  LDR R0, R6, #0
+
+  ; Modify map
+  STR R0, R3, #0
+
+  ; Restore registers
+  LDR R0, R6, #0
+  LDR R1, R6, #-1
+  LDR R2, R6, #-2
+  LDR R3, R6, #-3
+
+  ; Move stack
+  ADD R6, R6, #2  
 
   RET
 
