ef3eb85f3354c80144605b340da80025102bbb34
Chris Roberts <randomnameftw@gmail.com>
2015-04-16 00:35:43 -0500

    Add Move demonstration to draw_example1

--- draw_example1.asm
+++ draw_example1.asm
@@ -8,6 +8,8 @@ LDR R6, R6, #0
   LDR R5, R5, #0 ;Load pointer to Rand()
   LEA R4, UPDATE_ELEMENT
   LDR R4, R4, #0 ;Load pointer to UpdateElement()
+  LEA R3, MOVE
+  LDR R3, R3, #0 ;Load pointer to Move()
 
 ;----------------------------------------------
 ; First Call to Draw()
@@ -33,12 +35,23 @@ LDR R6, R6, #0
 ; Second Call to Draw()
 ;----------------------------------------------
   JSRR R5        ;draw()
-  
+  AND R0, R0, #0
+  ADD R0, R0, #5
+  STR R0, R6, #0    ;Push number 5 to stack
+  STR R0, R6, #-1   ;Push number 5 to stack
+
+  ADD R6, R6, #-2   ;sp = sp -2 ; move stack pointer to new base
+  ADD R0, R0, #-3   ;Move East
+  JSRR R3
+
+  JSRR R5        ;draw()
+
   HALT
 ;}//End of Main
 
 DRAW .FILL x3700 ;Address of Draw() routine
 UPDATE_ELEMENT .FILL x3730 ;Address of UpdateElement() routine
+MOVE .FILL x3800 ;Address of Move() routine
 SP  .FILL xFDFF ; Stack pointer to the end of user space MEM
 PLAYER_SYMBOL .FILL x70 ; 'P' for player
 .END
