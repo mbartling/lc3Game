2459030007ec4591ba89d341a2dfd3262c1e06e5
Chris Roberts <randomnameftw@gmail.com>
2015-04-16 00:33:06 -0500

    Make UpdateElement's functionality consistent with its description

--- UpdateElement.asm
+++ UpdateElement.asm
@@ -18,22 +18,22 @@ UpdateElement
   STR R1, R6, #-1  ; Save R1
   STR R2, R6, #-2  ; Save R2
   STR R3, R6, #-3  ; Save R3
-  LDR R0, R6, #1   ; Get y coordinate
-  LDR R1, R6, #2  ; Get x coordinate
+  LDR R0, R6, #1   ; Get x coordinate
+  LDR R1, R6, #2  ; Get y coordinate
   
   ; Compute final store address as: y * 9 + x
   AND R2, R2, #0
   ADD R2, R2, #9
 
-  ; Multiply y (R0) by 9 and store in R3
+  ; Multiply y (R1) by 9 and store in R3
   AND R3, R3, #0
 
-MULT	ADD R3, R3, R0
+MULT	ADD R3, R3, R1
 	ADD R2, R2, #-1
 	BRp MULT	
 
   ; Add offset (x)
-  ADD R3, R3, R1
+  ADD R3, R3, R0
 
   ; Add MAP location offset
   LEA R0, MAP
